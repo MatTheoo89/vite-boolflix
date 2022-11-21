560f89f1b748ac51ef5f02cfa023a163c5640536
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-21 11:02:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import axios from 'axios';
+import { store } from './data/store'
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+    components: { AppHeader, AppMain },
+  name: 'App',
+}
+</script>
+
+<template>
+
+  <AppHeader />
+  <AppMain />
+
+</template>
+
+
+<style lang="scss">
+
+  @use './styles/general';
+
+
+
+</style>
---
+++ src/components/AppSearchBar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppSearchBar',
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables.scss' as *;
+</style>
