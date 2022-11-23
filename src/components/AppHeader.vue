6254198a4c7440d88704db1d0607ad1fb7ba7ab4
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-23 14:16:50 +0100

    test searchbar bugged

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -21,19 +21,23 @@ export default {
                         @click="$emit('ricaricaPage')"
                         src="/logo-boolflix.png" alt="logo-boolflix">
                 </div>
-                <div class="col-6 offset-3 d-flex">
+
+                <div class="search-box col-5 offset-4 d-flex justify-content-end align-items-center">
+
                     <input
                         @keyup.enter="$emit('startSearch')"
                         v-model="store.inputToSearch"
-                        class="form-control me-4"
+                        class="form-control"
                         type="text"
                         placeholder="Search..."
                         aria-label="default input example">
-                    <button
-                        @click="$emit('startSearch')"
-                        type="button"
-                        class="btn btn-primary">Search
-                    </button>
+
+                            <button
+                                @click="$emit('startSearch')"
+                                type="button"
+                                class="search-btn">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
                 </div>
             </div>
         </div>
@@ -55,6 +59,44 @@ export default {
                     scale: (1.1);
                 }
             }
+        }
+    }
+
+    .search-box{
+        position: relative;
+        //background-color: aquamarine;
+        input{
+            position: absolute;
+            z-index: 1;
+            width: 30%;
+            border-radius: 70px;
+            border: none;
+            outline: none;
+            background-color: rgba(18, 18, 18, .6);
+            padding: 15px 0;
+            box-shadow: none;
+            transition: all 750ms;
+            text-indent: 20px;
+            &:focus{
+                width: 100%;
+            }
+            &::placeholder{
+                color: lighten($color-lightgrey, 40%);
+            }
+        }
+    }
+    button{
+        position: absolute;
+        z-index: 2;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        border: none;
+        outline: none;
+        background-color: $color-lightgrey;
+        color: #fff;
+        i{
+            font-size: 1.3rem;
         }
     }
 </style>
